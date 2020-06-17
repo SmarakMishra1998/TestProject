6f028d22d6b274ac75953771d31796f084d7177c
SMARAK MISHRA <SMARAK MISHRA@DESKTOP-D7UVU7M>
2020-06-17 17:32:03 +0530

    First Commit

---
+++ TestProjectForGitHub/src/com/trail/DemoClass.java
@@ -0,0 +1,16 @@
+package com.trail;
+
+public class DemoClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hey i am in main class");
+		
+		Smarak smarak = new Smarak();
+		
+		//Add anything here
+		
+		
+	}
+
+}
---
+++ TestProjectForGitHub/src/com/trail/Smarak.java
@@ -0,0 +1,9 @@
+package com.trail;
+
+public class Smarak {
+
+	public Smarak() {
+		System.out.println("Created by smarak");
+	}
+	
+}
